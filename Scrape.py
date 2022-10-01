683aac9508259e2ba9d1f207d7b25381f971f698
Ethan Ogle <encogle@gmail.com>
2022-10-01 14:33:07 -0400

    Adding list of recipes

---
+++ Scrape.py
@@ -0,0 +1,18 @@
+import re
+import bs4 as BeautifulSoup
+import requests
+
+def Scraping(WebUrl, function):
+    url = WebUrl
+    code = requests.get(url)
+    plain = code.text
+    soup = BeautifulSoup.BeautifulSoup(plain, 'html.parser')
+    for link in soup.findAll('a'):
+        recipe = link.get('href')
+        if "-recipe-" in recipe:
+            listOfRecipes.append(recipe)
+
+startUrl = "https://www.simplyrecipes.com/dinner-recipes-5091433"
+listOfRecipes = []
+Scraping(startUrl, 0)
+print(listOfRecipes)
