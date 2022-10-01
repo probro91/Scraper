4e80b498257464ac74b2ebec9e24339fa8343c3d
Ethan Ogle <encogle@gmail.com>
2022-10-01 16:04:50 -0400

    Consolidating

--- Scrape.py
+++ Scrape.py
@@ -7,15 +7,24 @@ def Scraping(WebUrl, function):
     code = requests.get(url)
     plain = code.text
     soup = BeautifulSoup.BeautifulSoup(plain, 'html.parser')
-    for link in soup.findAll('a'):
-        recipe = link.get('href')
-        if "-recipe-" in recipe:
-            listOfRecipes.append(recipe)
+    if function == "Getting recipes":
+        for link in soup.findAll('a'):
+            recipe = link.get('href')
+            if "-recipe-" in recipe:
+                listOfRecipes.append(recipe)
+        listOfRecipes.pop()
+        print(listOfRecipes)
+    if function == "Getting ingredients and title":
+        spans = soup.find_all("span", attrs={"data-ingredient-name":"true"})
+        recipeTitle = soup.find("h1", {"class": "heading__title"})
+        print(recipeTitle.text)
+        for ingredient in spans:
+            print(ingredient.text)
     
 
 startUrl = "https://www.simplyrecipes.com/dinner-recipes-5091433"
 listOfRecipes = []
-Scraping(startUrl, 0)
-print(listOfRecipes)
-
+Scraping(startUrl, "Getting recipes")
+for recipe in listOfRecipes:
+    Scraping(recipe, "Getting ingredients and title")
 
