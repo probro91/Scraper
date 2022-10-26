b835ff965f11c29f0f2a9e07643233ca3caf5263
Ethan Ogle <encogle@gmail.com>
2022-10-26 15:53:23 -0400

    Adding SQL and displaying to HTML

--- Scrape.py
+++ Scrape.py
@@ -1,66 +1,65 @@
-#import re
-from flask import Flask, render_template, request
-import bs4 as BeautifulSoup
-import requests
-import time
+import bs4 as BeautifulSoup # Parses HTMl
+import requests # Gets the HTML source code
 
-
-app = Flask(__name__)
-
-
-
-def Scraping(WebUrl):
-    #print("Time before recipes" + str(time.time_ns()))
+def Scraping(WebUrl): # Gets all the recipes for a meal type, along with their url, image url, title, and ingredients
     url = WebUrl
     code = requests.get(url)
     plain = code.text
     soup = BeautifulSoup.BeautifulSoup(plain, 'html.parser')
-    for link in soup.findAll('a'):
+
+    #Finds all the liks to the recipes and adds them to a list
+    for link in soup.findAll('a'): 
         recipe = link.get('href')
         if "-recipe-" in recipe:
             listOfRecipes.append(recipe)
     listOfRecipes.pop()
-    #print("Time after recipes" + str(time.time_ns()))
+    #Parses through all the recipes and gets the ingredeints, image url, and title
     for recipe in listOfRecipes:
         code = requests.get(recipe).text
         soupIngredient = BeautifulSoup.BeautifulSoup(code, 'html.parser')
         listOfIngredients = []
+        listOfIngredients.append(recipe)
         spans = soupIngredient.find_all("span", attrs={"data-ingredient-name":"true"})
         recipeTitle = soupIngredient.find("h1", {"class": "heading__title"})
         listOfIngredients.append(recipeTitle.text)
         for imgLink in soupIngredient.findAll('img'):
-            image = imgLink.get('src')
+            image = (str)(imgLink.get('src'))
             if "-LEAD-" in image:
                 listOfIngredients.append(image)
+                break
         for ingredient in spans:
             listOfIngredients.append(ingredient.text)
         eachUrl.update({recipe:listOfIngredients})
-    #print("Time after ingredients" + str(time.time_ns()))
     
 eachUrl = {}
-mealType = (str)(request.form["mealType"])
+mealType = input("What meal type is it? ")
 if mealType.lower() == 'breakfast':
     startUrl = "https://www.simplyrecipes.com/breakfast-recipes-5091541"
+    mealType = "Breakfast"
 elif mealType.lower() == 'lunch':
     startUrl = "https://www.simplyrecipes.com/lunch-recipes-5091263"
+    mealType = "Lunch"
 elif mealType.lower() == 'dinner':
     startUrl = "https://www.simplyrecipes.com/dinner-recipes-5091433"
+    mealType = "Dinner"
 elif mealType.lower() == 'dessert':
     startUrl = "https://www.simplyrecipes.com/dessert-recipes-5091513"
-elif mealType.lower() == 'snacks&apps':
+    mealType = "Dessert"
+elif mealType.lower() == 'snacks and appitizers':
     startUrl = "https://www.simplyrecipes.com/snacks-and-appetizer-recipes-5090762"
+    mealType = "SnacksAndApps"
 else:
     startUrl = ""
-startIngredients = ((str)(request.form["Ingredients"])).split(", ")
+
+startIngredients = []
+startIngredients = (input("Which ingredients do you have? ")).split(", ")
 listOfRecipes = []
+matchingRecipes = {}
 Scraping(startUrl)
+#Adds only the recipes with matching ingredietns to the dictionary
 for recipe in eachUrl:
-    if not all(item in eachUrl[recipe] for item in startIngredients):
-        eachUrl.delete(recipe)
+    if all(item in eachUrl[recipe] for item in startIngredients):
+        matchingRecipes.update({recipe:eachUrl[recipe]})
 
-#print("Time after compairing" + str(time.time_ns()))
-
-#print(eachUrl)
-@app.route("/", methods=["GET", "POST"])
-def home():
-    return render_template("base.html", ingredients=eachUrl)
+for recipe in matchingRecipes.values():
+    print (recipe)
--- website/views.py
+++ website/views.py
@@ -1,71 +1,57 @@
-#from django.shortcuts import render
-from flask import Blueprint, render_template, request
-import bs4 as BeautifulSoup
-import requests
-import time
+from flask import Blueprint, render_template, request # Python integration with making websites
+import mysql.connector # Allows Python to execute SQL commands
 
-#from soupsieve import match
+# Connects to mySQL database and server
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="root",
+    database="ScrapsRecipes"
+    )
 
-eachUrl = {}
-listOfRecipes = []
-matchingRecipes = {}
+mycursor = mydb.cursor(buffered=True)
 
+result = ""
+data = ""
 
-def Scraping(WebUrl):
-    #print("Time before recipes" + str(time.time_ns()))
-    url = WebUrl
-    code = requests.get(url)
-    plain = code.text
-    soup = BeautifulSoup.BeautifulSoup(plain, 'html.parser')
-    for link in soup.findAll('a'):
-        recipe = link.get('href')
-        if "-recipe-" in recipe:
-            listOfRecipes.append(recipe)
-    listOfRecipes.pop()
-    #print("Time after recipes" + str(time.time_ns()))
-    for recipe in listOfRecipes:
-        code = requests.get(recipe).text
-        soupIngredient = BeautifulSoup.BeautifulSoup(code, 'html.parser')
-        listOfIngredients = []
-        listOfIngredients.append(recipe)
-        spans = soupIngredient.find_all("span", attrs={"data-ingredient-name":"true"})
-        recipeTitle = soupIngredient.find("h1", {"class": "heading__title"})
-        listOfIngredients.append(recipeTitle.text)
-        for imgLink in soupIngredient.findAll('img'):
-            image = (str)(imgLink.get('src'))
-            if "-LEAD-" in image:
-                listOfIngredients.append(image)
-                break
-        for ingredient in spans:
-            listOfIngredients.append(ingredient.text)
-        eachUrl.update({recipe:listOfIngredients})
-    #print("Time after ingredients" + str(time.time_ns()))
-    
+htmlCode = []
 
+tbl = "<tr><td>URL</td><td>Title</td><td>Image</td><td>Ingredients</td></tr>"
+htmlCode.append(tbl)
+"""
+Creates the database:
+mycursor.execute("CREATE DATABASE ScrapsRecipes") 
 
+Creates the tables for each meal type:
+mealTypes = ["Breakfast", "Lunch", "Dinner", "Dessert", "SnacksAndApps"]
+for mealType in mealTypes:
+    mycursor.execute("CREATE TABLE " + mealType + " (url VARCHAR(200), 
+    name VARCHAR(150), 
+    imageUrl VARCHAR(300), 
+    ingredients VARCHAR(500), 
+    recipeID int PRIMARY KEY AUTO_INCREMENT)")
+
+Adds all the recipes and their ingredients into their corresponding tables:
+for mealType in mealTypes:
+    for recipe in eachUrl:
+        mycursor.execute("INSERT INTO " + mealType + " (url, name, imageUrl, ingredients) 
+        VALUES (%s,%s,%s,%s)", (recipe,eachUrl[recipe][1],eachUrl[recipe][2],str(eachUrl[recipe][3:])))
+"""
 
 views = Blueprint('views', __name__)
+
 
 @views.route('/', methods=["GET","POST"])
 def home():
     if request.method == "POST":
         mealType = (str)(request.form["mealType"])
-        if mealType.lower() == 'breakfast':
-            startUrl = "https://www.simplyrecipes.com/breakfast-recipes-5091541"
-        elif mealType.lower() == 'lunch':
-            startUrl = "https://www.simplyrecipes.com/lunch-recipes-5091263"
-        elif mealType.lower() == 'dinner':
-            startUrl = "https://www.simplyrecipes.com/dinner-recipes-5091433"
-        elif mealType.lower() == 'dessert':
-            startUrl = "https://www.simplyrecipes.com/dessert-recipes-5091513"
-        elif mealType.lower() == 'snacks&apps':
-            startUrl = "https://www.simplyrecipes.com/snacks-and-appetizer-recipes-5090762"
-        else:
-            startUrl = ""
         startIngredients = []
         startIngredients = ((str)(request.form["Ingredients"])).split(", ")
-        Scraping(startUrl)
-        for recipe in eachUrl:
-            if all(item in eachUrl[recipe] for item in startIngredients):
-                matchingRecipes.update({recipe:eachUrl[recipe]})
-    return render_template("base.html", ingredients=matchingRecipes)
+        sql = "SELECT * FROM " + mealType + " WHERE ingredients LIKE '%" + startIngredients[0] + "%'"
+        for x in range(1,len(startIngredients)):
+            sql += " AND ingredients LIKE '%" + startIngredients[x] + "%'"
+        mycursor.execute(sql)
+        return render_template("base.html", output_data = mycursor)
+    if request.method == "GET":
+        mycursor.execute("SELECT * FROM Breakfast")
+        return render_template("base.html", output_data = [])
